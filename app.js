4b17929343fc942c5c16341726a1085fb9efc2a1
ybinstock <ybinstock@gmail.com>
2017-08-28 17:05:53 +0200

    first

---
+++ app.js
@@ -0,0 +1,125 @@
+AFRAME.registerComponent('berlin', {
+  init: function() {
+    var sphere = document.getElementById('Berlin');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Berlin.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('boston', {
+  init: function() {
+    var sphere = document.getElementById('Boston');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Boston.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('buenosaires', {
+  init: function() {
+    var sphere = document.getElementById('BuenosAires');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/BuenosAires.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('delhi', {
+  init: function() {
+    var sphere = document.getElementById('Delhi');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Delhi.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('hongkong', {
+  init: function() {
+    var sphere = document.getElementById('HongKong');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/HongKong.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('london', {
+  init: function() {
+    var sphere = document.getElementById('London');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/London.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('ny', {
+  init: function() {
+    var sphere = document.getElementById('NY');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/NY.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('paris', {
+  init: function() {
+    var sphere = document.getElementById('Paris');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Paris.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('sf', {
+  init: function() {
+    var sphere = document.getElementById('SF');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/SF.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('sydney', {
+  init: function() {
+    var sphere = document.getElementById('Sydney');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Sydney.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('tokyo', {
+  init: function() {
+    var sphere = document.getElementById('Tokyo');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Tokyo.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('tunis', {
+  init: function() {
+    var sphere = document.getElementById('Tunis');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Tunis.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('vancouver', {
+  init: function() {
+    var sphere = document.getElementById('Vancouver');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Vancouver.jpg");
+    });
+  }
+});
+
+AFRAME.registerComponent('washington', {
+  init: function() {
+    var sphere = document.getElementById('Washington');
+    sphere.addEventListener('click', function(evt) {
+      document.getElementById('sky').setAttribute("src", "panos/Washington.jpg");
+    });
+  }
+});
